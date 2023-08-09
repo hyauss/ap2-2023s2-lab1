a363fc6c2cefcfc0d36821de9bb4b476046e49b8
hyauss <69730958+hyauss@users.noreply.github.com>
2023-08-09 20:33:09 -0300

    Create exemplo1.c

---
+++ exemplo1.c
@@ -0,0 +1,20 @@
+/*
+*Lab.1
+*Exemplo 1
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  //imprime 	algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritimos e ");
+	printf("Prog. II\n");
+	
+	
+	//pausa antes de terminar o programa
+	system ("pause");
+	return 0;
+}
